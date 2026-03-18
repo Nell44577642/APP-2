82eb6a3cdf8cee70152a9de7f9168e2386684cad
Nell44577642 <nell.ribeiro@esme.fr>
2026-03-18 10:50:05 +0100

    Implement Node class for binary search tree
    Added a Node class for a binary search tree with price and player attributes.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+import tkinter as tk
+from tkinter import ttk
+import random
+
+# BST NODE
+
+class Node:
+    def __init__(self, price):
+        self.price = price
+        self.players = []
+        self.left = None
+        self.right = None
